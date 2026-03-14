bd7b8ec52f5d3976679b11e66562976f5438b00a
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2026-03-15 00:58:20 +0300

    Add footer text and site year to template context; create new Mustache template for single column layout

--- public/theme/mru_odel/layout/columns1.php
+++ public/theme/mru_odel/layout/columns1.php
@@ -3,10 +3,16 @@
 defined('MOODLE_INTERNAL') || die();
 
 $bodyattributes = $OUTPUT->body_attributes([]);
+
+$theme = theme_config::load('mru_odel');
+$footertext = !empty($theme->settings->footertext) ? format_text($theme->settings->footertext, FORMAT_HTML) : '';
+
 $templatecontext = [
     'sitename'       => format_string($SITE->shortname, true, ['context' => context_course::instance(SITEID), "escape" => false]),
     'output'         => $OUTPUT,
     'bodyattributes' => $bodyattributes,
+    'footertext'     => $footertext,
+    'siteyear'       => date('Y'),
 ];
 
 if (empty($PAGE->layout_options['noactivityheader'])) {
@@ -15,4 +21,4 @@ if (empty($PAGE->layout_options['noactivityheader'])) {
     $templatecontext['headercontent'] = $header->export_for_template($renderer);
 }
 
-echo $OUTPUT->render_from_template('theme_boost/columns1', $templatecontext);
+echo $OUTPUT->render_from_template('theme_mru_odel/columns1', $templatecontext);
